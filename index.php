6afee6760955e300be24808bc44a038906a6f1b1
Truc Nguyen <duongtruc.92@gmail.com>
2015-04-04 09:12:28 +0700

    Updated project

--- index.php
+++ index.php
@@ -9,19 +9,18 @@
  * @link https://github.com/panique/php-mvc/
  * @license http://opensource.org/licenses/MIT MIT License
  */
- /*Hello, may tinh nay da duoc update*/
 
 // load the (optional) Composer auto-loader
 if (file_exists('vendor/autoload.php')) {
     require 'vendor/autoload.php';
 }
 
+// load application config (error reporting etc.)
+require 'application/config/config.php';
 
 // load application class
 require 'application/libs/application.php';
-if (file_exists('vendor/autoload.php')) {
-    require 'vendor/autoload.php';
-echo"Duc Hieu"
-}
+require 'application/libs/controller.php';
 
-
+// start the application
+$app = new Application();
